4ad6262ddb852f0fdd6849e929c41ed1c1e69dd2
Aleksandr Uryadov <uryadov212@gmail.com>
2020-06-21 22:26:20 +0400

    Headline Component with tests

---
+++ src/component/headline/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import './styles.scss'
+
+class Headline extends Component {
+    render() {
+        const { header, desc } = this.props
+
+        if(!header){
+            return null;
+        }
+
+        return(
+            <div data-test="HeadlineComponent">
+                <h1 data-test="header">{header}</h1>
+                <p data-test="description">
+                    {desc}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Headline;
