9e9c45b71d7ddc1679d7b1e4b9b8b88173c1c500
URYADOV Aleksandr <auryadov@iom.int>
2020-06-22 11:59:35 +0400

    Added propTypes to component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import Header from './component/header'
-import Headline from './component/headline'
-import './app.scss'
+import React from "react";
+import Header from "./component/header";
+import Headline from "./component/headline";
+import "./app.scss";
+
+const tempArr = [
+  {
+    fName: "Alex",
+    lName: "Uryadov",
+    email: "alex@gmail.com",
+    age: 34,
+    onlineStatus: true,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
       <section className="main">
-        <Headline header="Posts" desc="Click the button to render the posts!"/>
+        <Headline
+          header="Posts"
+          desc="Click the button to render the posts!"
+          tempArr={tempArr}
+        />
       </section>
     </div>
   );
--- src/component/headline/headline.test.js
+++ src/component/headline/headline.test.js
@@ -1,54 +1,71 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Headline from '../headline'
-import { findByTestAtrr } from '../../../Utils'
+import React from "react";
+import { shallow } from "enzyme";
+import Headline from "../headline";
+import { findByTestAtrr, checkProps } from "../../../Utils";
 
 const setUp = (props = {}) => {
-    const component = shallow(<Headline {...props}/>)
-    return component
-}
+  const component = shallow(<Headline {...props} />);
+  return component;
+};
 
-describe('Headline Component', () => {
+describe("Headline Component", () => {
+  describe("check Prop Types", () => {
+    it("Shoul not throw warning", () => {
+      const expectedProps = {
+        header: "Test header",
+        desc: "Test desc",
+        tempArr: [
+          {
+            fName: "Alex",
+            lName: "Uryadov",
+            email: "alex@gmail.com",
+            age: 34,
+            onlineStatus: true,
+          },
+        ],
+      };
+      const propsErr = checkProps(Headline, expectedProps);
 
-    describe('Have props', () => {
+      expect(propsErr).toBeUndefined();
+    });
+  });
+  describe("Have props", () => {
+    let wrapper;
 
-        let wrapper
+    beforeEach(() => {
+      const props = {
+        header: "Test header",
+        desc: "Test description",
+      };
+      wrapper = setUp(props);
+    });
 
-        beforeEach(() => {
-            const props = {
-                header: 'Test header',
-                desc: 'Test description'
-            }
-            wrapper = setUp(props)
-        })
+    it("should render without errors", () => {
+      const component = findByTestAtrr(wrapper, "HeadlineComponent");
+      expect(component.length).toBe(1);
+    });
 
-        it('should render without errors', () => {
-            const component = findByTestAtrr(wrapper, 'HeadlineComponent')
-            expect(component.length).toBe(1)
-        })
+    it("shoul render H1", () => {
+      const component = findByTestAtrr(wrapper, "header");
+      expect(component.length).toBe(1);
+    });
 
-        it('shoul render H1', () => {
-            const component = findByTestAtrr(wrapper, 'header')
-            expect(component.length).toBe(1)
-        })
+    it("should render desc", () => {
+      const component = findByTestAtrr(wrapper, "description");
+      expect(component.length).toBe(1);
+    });
+  });
 
-        it('should render desc', () => {
-            const component = findByTestAtrr(wrapper, 'description')
-            expect(component.length).toBe(1)
-        })
-    })
+  describe("Have NO props", () => {
+    let wrapper;
 
-    describe('Have NO props', () => {
+    beforeEach(() => {
+      wrapper = setUp();
+    });
 
-        let wrapper
-        
-        beforeEach(() => {
-            wrapper = setUp()
-        })
-
-        it('should not render', () => {
-            const component = findByTestAtrr(wrapper, 'HeadlineComponent')
-            expect(component.length).toBe(0)
-        })
-    })
-})  
+    it("should not render", () => {
+      const component = findByTestAtrr(wrapper, "HeadlineComponent");
+      expect(component.length).toBe(0);
+    });
+  });
+});
